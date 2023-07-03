72e267ad0c330552f219635fe4e62fc437f11c7c
Evgeniy <lukik884@gamil.com>
2023-07-03 13:59:47 +0300

    Сделал домашку ко второму семинару

---
+++ seminar_2/HomeWork/task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (Math.Abs(number) > 99)
+{
+    while (Math.Abs(number) >= 1000)
+    {
+        number = number/10;
+    }
+
+    Console.WriteLine($"Третья цифра числа {Math.Abs(number%10)}"); 
+}
+else Console.WriteLine("Третьей цифры нет");
---
+++ seminar_2/HomeWork/task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number >= 1) && (number <= 7))
+{
+    if ((number==6)||(number==7))
+    {
+        Console.WriteLine("Да, это выходной!");
+    }
+    else Console.WriteLine("Иди работай!");
+}
+else Console.WriteLine("Читать умеешь?!");
