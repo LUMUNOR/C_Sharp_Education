b3175c086b0992eb4958e76abe9bb5b6229be39c
Evgeniy <lukik884@gamil.com>
2023-07-13 13:54:10 +0300

    Домашка к семинару №6

---
+++ seminar_6/HomeWork/task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Метод запроса числа
+int Request (string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int m = Request("Сколько чиесел хотите ввести? - ");
+
+Console.WriteLine("Приступим кo вводу!");
+
+int counter = 0;
+int number = 0;
+
+for (int i=0; i<m; i++)
+{
+    number = int.Parse(Console.ReadLine());
+    if (number > 0) counter++;
+}
+
+Console.WriteLine($"Вы ввели {counter} положительных чисел(a).");
---
+++ seminar_6/HomeWork/task_43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Метод запроса числа
+int Request (string massage)
+{
+    Console.Write(massage);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int b1 = Request("Введите коэффециент b1 = ");
+int k1 = Request("Введите коэффециент k1 = ");
+int b2 = Request("Введите коэффециент b2 = ");
+int k2 = Request("Введите коэффециент k2 = ");
+
+double x = (b2-b1)/(k1-k2);
+double y = (k1*x)+b1;
+
+Console.Write($"Координата пересечния двух примых ({x}, {y})");
